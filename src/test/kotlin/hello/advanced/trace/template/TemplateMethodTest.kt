3fab87a2080eb33056f34d1f747909e4e94f0736
HoYoung <kua@kakao.com>
2023-03-28 21:58:03 +0900

    익명 내부 클래스 사용

--- src/test/kotlin/hello/advanced/trace/template/TemplateMethodTest.kt
+++ src/test/kotlin/hello/advanced/trace/template/TemplateMethodTest.kt
@@ -1,9 +1,11 @@
 package hello.advanced.trace.template
 
+import hello.advanced.trace.template.code.AbstractTemplate
 import hello.advanced.trace.template.code.SubClassLogic1
 import hello.advanced.trace.template.code.SubClassLogic2
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
+
 
 class TemplateMethodTest {
     private val log = LoggerFactory.getLogger(javaClass)
@@ -39,6 +41,23 @@ class TemplateMethodTest {
         val template1 = SubClassLogic1()
         template1.execute()
         val template2 = SubClassLogic2()
+        template2.execute()
+    }
+
+    @Test
+    fun templateMethodV2() {
+        val template1: AbstractTemplate = object : AbstractTemplate() {
+            override fun call() {
+                log.info("비즈니스 로직1 실행")
+            }
+        }
+        template1.execute()
+
+        val template2: AbstractTemplate = object : AbstractTemplate() {
+            override fun call() {
+                log.info("비즈니스 로직2 실행")
+            }
+        }
         template2.execute()
     }
 
